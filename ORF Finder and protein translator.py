36b2647b98776965e1e735bbfa7727ce04584786
Pamela Cruz <32467856+pamelacruzbr@users.noreply.github.com>
2023-07-11 23:53:41 +0200

    ORF identification and translation to protein(in progress).

--- ORF Finder and protein translator.py
+++ ORF Finder and protein translator.py
@@ -45,3 +45,57 @@ parse_sequences(1)
 # Identify ALL ORFS (ATG.. TAA,TAG,TGA
 # Length of ORF, longest and its ID
 # A given ID, find longest ORF, find its start position (frame)
+
+""" Regex solution"""
+
+# First step, parse in the sequences
+# Import Biopython library
+from typing import List
+
+from Bio import SeqIO
+import re
+
+ids = []
+sequences = []
+lengths = []
+
+# Parse FASTA file
+
+for seq_record in SeqIO.parse("dna.example.fasta", "fasta"):
+        ids.append(seq_record.id) # List all IDs
+        sequences.append(seq_record.seq) # List all sequences
+        lengths.append(len(seq_record)) # List all lengths
+
+sequencestr = str(sequences)
+
+pattern = re.compile(r'(ATG(?:...)*?(?:TAG|TGA|TAA))')
+
+ORF_total = []
+
+def ORF_finder(frame=0):
+    if frame > 2 or frame < 0:
+        print("Error: frame has to be between 0 and 2")
+    else:
+        for seq in sequences:
+            strand_positve = pattern.findall(str(seq[frame:]))
+            revcomp = seq.reverse_complement()
+            strand_negative = pattern.findall(str(revcomp[frame:]))
+            ORF_total.append(strand_positve + strand_negative)
+    return ORF_total
+
+ORF_finder(2)
+print(len(ORF_total))
+
+for ORF in ORF_total:
+                print
+                ident = print(
+                ids[sequences.index(seq)])
+                for ORF in ORF_total:
+                    print('\nTotal ORFS = ',cnt,'\n', ORF,'\nlenght = ', len(ORF),'\n---------------------------\n\n')
+
+def ORF_informations():
+    ORF_list = ORF_finder(2)
+    max_ORF = max(ORF_list)
+    print(len(max_ORF))
+
+ORF_informations()
