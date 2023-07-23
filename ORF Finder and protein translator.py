4265ea03ae047d308185ac898f803bd1d1b6cd58
Pamela Cruz <32467856+pamelacruzbr@users.noreply.github.com>
2023-07-23 23:43:00 +0200

    ORF identification full project plus router.

--- ORF Finder and protein translator.py
+++ ORF Finder and protein translator.py
@@ -16,11 +16,33 @@ def import_fasta(file="dna.example.fasta"):
             sequences.append(seq_record.seq) # List all sequences
             lengths.append(len(seq_record)) # List all lengths
     seq_fasta = dict(map(lambda gene,seq : (gene,seq) , ids, sequences))
-    return seq_fasta
+    return {
+        'seq_fasta': seq_fasta,
+        'ids': ids,
+        'lengths': lengths,
+        'sequences': sequences
+    }
+
+def count_seqmaxmin():
+    lengths = import_fasta()['lengths']
+    ids = import_fasta()['ids']
+    max_seq = max(lengths)
+    min_seq = min(lengths)
+    print("Your FASTA file has {} entries \n ----------------- \n".format(len(ids)))
+    print("The maximum length of sequence found in this FASTA is : %d bp \n ----------------- \n" %max(lengths)) #Print MAX seq
+    print("The minimum length of sequence found in this FASTA is : %d bp" %min(lengths)) #Print MIN seq
+    if lengths.count(max(lengths))> 1: #Count if there are more than 1 seq with the MAX value
+        print("There are %d with %d basepairs, the ID are: {}".format((ids[lengths.index(max_seq)])) %(lengths.count(max_seq), max_seq))
+    else:
+        print("There is only one sequence with %d basepairs, its ID is: {}".format((ids[lengths.index(max_seq)])) %max_seq)
+    if lengths.count(min(lengths))> 1: #Count if there are more than 1 seq with the MIN value
+        return print("There are %d with %d basepairs, the ID are: {}".format((ids[lengths.index(min_seq)])) %(lengths.count(min_seq), max_seq))
+    else:
+        print("There is only one sequence with %d basepairs, its ID is: {} \n ----------------- \n".format((ids[lengths.index(min_seq)])) %min_seq)
 
 
-def ORF_finder2(frame=3):
-    seq_fasta = user_choice()['data']
+def ORF_finder2(data, frame=3):
+    seq_fasta = data
     pattern = re.compile(r'((?:...)*?)(ATG(?:...)*?(?:TAG|TGA|TAA))')
     lengthorf = 1
     orf = {
@@ -42,7 +64,7 @@ def ORF_finder2(frame=3):
                     start = match.start() + len(group1)
                     end = 0 + match.end()
                     length = len(group2)
-                    if length >= lengthorf:
+                    if length >= lengthorf and length < 2000:
                         lengthorf = length
                         orf = {
                             'length': length,
@@ -59,7 +81,7 @@ def ORF_finder2(frame=3):
                     start = match.start() + len(group1)
                     end = 0 + match.end()
                     length = len(group2)
-                    if length >= lengthorf:
+                    if length >= lengthorf and length < 2000:
                         lengthorf = length
                         orf = {
                             'length': length,
@@ -77,13 +99,27 @@ def user_choice():
     typeseq = input("Single seq or FASTA ? (seq or FASTA)")
     if typeseq == 'FASTA':
         file = input('Give the name of the FASTA file')
-        data = import_fasta(file)
+        data = import_fasta(file)['seq_fasta']
     elif typeseq == 'Single seq':
         data = {}
+        identif = ''
         identif = input('Give the name of the gene')
-        seq_fasta = import_fasta()
-        data[identif] = seq_fasta.get(identif, "identif is not a key")
+        seq_fasta = import_fasta()['seq_fasta']
+        data[identif] = seq_fasta.get(identif)
     else: print('error')
     return data
 
-print(user_choice())
+
+def ORF_informations():
+    data = user_choice()
+    sequence = list(data.values())[0]
+    if len(data) == 1:
+        print(" Your sequence has a length of: {}".format(len(sequence)))
+    else:
+        count_seqmaxmin()
+    print("Longest ORF in reading frame 1: {}, its start position is: {}, id of the gene: {} \n ".format(ORF_finder2(data, frame=1)['length'], ORF_finder2(data, frame=1)['start'], ORF_finder2(data, frame=1)['name']))
+    print("Longest ORF in reading frame 2: {}, its start position is: {}, id of the gene: {} \n ".format(ORF_finder2(data, frame=2)['length'], ORF_finder2(data, frame=2)['start'], ORF_finder2(data, frame=2)['name']))
+    print("Longest ORF in reading frame 3: {}, its start position is: {}, id of the gene: {} \n ".format(ORF_finder2(data, frame=3)['length'], ORF_finder2(data, frame=3)['start'], ORF_finder2(data, frame=3)['name']))
+
+
+ORF_informations()
