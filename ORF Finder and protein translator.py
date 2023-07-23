fcdb013f4e76d2f2fac3a9c965908a87b4f95c6d
Pamela Cruz <32467856+pamelacruzbr@users.noreply.github.com>
2023-07-23 21:06:19 +0200

    ORF identification and translation to protein(in progress).

--- ORF Finder and protein translator.py
+++ ORF Finder and protein translator.py
@@ -1,30 +1,27 @@
+""" Regex solution 2"""
+
 # First step, parse in the sequences
 # Import Biopython library
 from typing import List
-
 from Bio import SeqIO
 import re
 
-ids = []
-sequences = []
-lengths = []
-
 # Parse FASTA file
-
-for seq_record in SeqIO.parse("dna.example.fasta", "fasta"):
-        ids.append(seq_record.id) # List all IDs
-        sequences.append(seq_record.seq) # List all sequences
-        lengths.append(len(seq_record)) # List all lengths
-
-
-seq_fasta = dict(map(lambda gene,seq : (gene,seq) , ids, sequences))
-
-
-
-pattern = re.compile(r'((?:...)*?)(ATG(?:...)*?(?:TAG|TGA|TAA))')
+def import_fasta(file="dna.example.fasta"):
+    ids = []
+    sequences = []
+    lengths = []
+    for seq_record in SeqIO.parse(file, "fasta"):
+            ids.append(seq_record.id) # List all IDs
+            sequences.append(seq_record.seq) # List all sequences
+            lengths.append(len(seq_record)) # List all lengths
+    seq_fasta = dict(map(lambda gene,seq : (gene,seq) , ids, sequences))
+    return seq_fasta
 
 
 def ORF_finder2(frame=3):
+    seq_fasta = user_choice()['data']
+    pattern = re.compile(r'((?:...)*?)(ATG(?:...)*?(?:TAG|TGA|TAA))')
     lengthorf = 1
     orf = {
         'length': 1,
@@ -33,6 +30,7 @@ def ORF_finder2(frame=3):
         'name': '',
         'strand': ''
     }
+
     if frame > 3 or frame <= 0:
         print("Error: frame has to be between 0 and 2")
     else:
@@ -72,3 +70,20 @@ def ORF_finder2(frame=3):
                             'strand': 'negative'
                         }
         return orf
+
+#ROUTER !!
+def user_choice():
+    file = ''
+    typeseq = input("Single seq or FASTA ? (seq or FASTA)")
+    if typeseq == 'FASTA':
+        file = input('Give the name of the FASTA file')
+        data = import_fasta(file)
+    elif typeseq == 'Single seq':
+        data = {}
+        identif = input('Give the name of the gene')
+        seq_fasta = import_fasta()
+        data[identif] = seq_fasta.get(identif, "identif is not a key")
+    else: print('error')
+    return data
+
+print(user_choice())
